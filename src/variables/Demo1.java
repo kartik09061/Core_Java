a01cbcdf14e43ece5aee0ead2d88abcef2bab5e4
Your Namekartik09061 <kartikpatil723@gmail.com>
2026-07-04 19:39:04 +0530

    variables: programs

---
+++ src/variables/Demo1.java
@@ -0,0 +1,9 @@
+package variables;
+
+public class Demo1 {
+public static void main(String[] args){
+    int age = 24;
+    System.out.println(age);
+}
+
+}
---
+++ src/variables/Demo2.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class Demo2 {
+    public static void main (String[] args){
+        byte range = 20;
+        int id = 23456789;
+        short employee = 34;
+        System.out.println(range);
+        System.out.println(id);
+        System.out.println(employee);
+    }
+}
