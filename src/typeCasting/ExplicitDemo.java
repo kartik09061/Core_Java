2c1e858e011dd2607b52bcb950a94dd4ea2a9aa3
Your Namekartik09061 <kartikpatil723@gmail.com>
2026-07-05 11:55:28 +0530

    typecasting : programs

---
+++ src/typeCasting/ExplicitDemo.java
@@ -0,0 +1,9 @@
+package typeCasting;
+
+public class ExplicitDemo {
+    static void main(String[] args) {
+        int num = 1001;
+        byte num1 =(byte)num;
+        System.out.println(num1);
+    }
+}
---
+++ src/typeCasting/ImplicitDemo.java
@@ -0,0 +1,11 @@
+package typeCasting;
+
+public class ImplicitDemo {
+    static void main(String[] args){
+        int num = 101;
+        double num1 = num;
+        System.out.println("Value of num1 ="+num);
+        System.out.println(num1);
+
+    }
+}
